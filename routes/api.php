15a3a0267a3a6efd90d08fc6414ca01a114021dc
milena_20 <milena.tavares@maiatecnologia.com.br>
2024-01-26 09:31:44 -0300

    aula 04 - Configurações iniciais para trabalhar com APIs no laravel

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return response()->json(['message' => 'Success']);
+    return response()->json(['message' => 'Success'], 200);
 });
